569b6461b69fc7e5647e5af465ff9d9f26366bc1
goodee <goodee@mainserver>
2018-06-11 15:01:33 +0900

    파일서버 업로드

---
+++ src/main/java/com/java/file/service/FileServiceInterface.java
@@ -0,0 +1,13 @@
+package com.java.file.service;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileServiceInterface {
+	
+	public HashMap<String, Object> fileUpload(MultipartFile[] files, String dir, HttpServletRequest req);
+
+}
