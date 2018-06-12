71eaf50bdd7597412b109a3b385324e2bfe82318
goodee <goodee@mainserver>
2018-06-12 11:41:29 +0900

    파일 업로드 DB연결 적용

--- src/main/java/com/java/file/service/FileService.java
+++ src/main/java/com/java/file/service/FileService.java
@@ -9,16 +9,25 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.java.file.dao.FileDaoInterface;
+import com.java.util.HttpUtil;
+
 @Service
 public class FileService implements FileServiceInterface {
-
+	
+	@Autowired
+	FileDaoInterface FDI;
+	
 	@Override
 	public HashMap<String, Object> fileUpload(MultipartFile[] files, String dir, HttpServletRequest req) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+		
+		HashMap<String, Object> param = HttpUtil.getParamMap(req); // 유저넘버 게시판넘버를 받아온다.
 		
 		for(int i = 0; i < files.length; i++) {
 			HashMap<String, Object> fileMap = new HashMap<String, Object>();
@@ -27,6 +36,7 @@ public class FileService implements FileServiceInterface {
 			try {
 				byte[] bytes = files[i].getBytes();
 				String path = "/var/www/html/resources/" + dir + "/";
+//				String path = req.getSession().getServletContext().getRealPath("/") + "resources/" + dir + "/";
 				String dns = "http://gudi.iptime.org:10090/";
 				
 				File dirF = new File(path);
@@ -42,7 +52,18 @@ public class FileService implements FileServiceInterface {
 				
 				fileMap.put("fileName", fileNm);
 				fileMap.put("filePath", path);
-				fileMap.put("fileUrl", dns + "resources/" + dir + "/" + fileNm);
+				fileMap.put("fileURL", dns + "resources/" + dir + "/" + fileNm);
+				
+				fileMap.put("boardNo", param.get("boardNo"));
+				fileMap.put("userNo", param.get("userNo"));
+				
+				/*************************************************************************/
+				// DB 연결부분
+				FDI.insert(fileMap); // fileMap이 Dao의 param 부분에 들어가야 하므로, fileMap.put 부분을 미리 써 받은 파라미터 값들을 보내준다.
+				 
+				  
+				/*************************************************************************/
+				
 				list.add(fileMap);				
 				
 			} catch (Exception e) {
--- src/main/webapp/WEB-INF/views/main.jsp
+++ src/main/webapp/WEB-INF/views/main.jsp
@@ -5,7 +5,7 @@
 </head>
 <body>
 <h1>
-	Hello File Server!  !
+	종화 파일서버.
 </h1>
 </body>
 </html>
